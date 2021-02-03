0c420e628b46457c2757cffe2998e088790e2392
Nagato <67494727+NagatoDEV@users.noreply.github.com>
2021-02-03 10:03:44 -0500

    Add files via upload

---
+++ JamesJustinCalderon.cpp
@@ -0,0 +1,28 @@
+﻿// James Justin Calderon
+// 2/2/2021
+// C++ PE Assignment
+// *********************************************************************
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	cout << "^*^*^*^*^*^*^*^*^*^*^*^*^^*^*^*^*^*^*^*^*^*^*^*^*^" << endl;
+	cout << "         C++ PE Assignment Name Project           " << endl; 
+	cout << "                                                  " << endl; 
+    cout << "           _                                      " << endl;
+    cout << "          | |                                     " << endl;
+    cout << "          | | __ _ _ __ ___   ___  ___            " << endl;
+    cout << "      _   | |/ _` | '_ ` _ \ / _ \/ __|           " << endl;
+    cout << "     | |__| | (_| | | | | | |  __/\__ \           " << endl; 
+    cout << "      \____/ \__,_|_| |_| |_|\___||___/           " << endl; 
+    cout << "                                                  " << endl;
+    cout << "                                                  " << endl; 
+	cout << "^*^*^*^*^*^*^*^*^*^*^*^*^^*^*^*^*^*^*^*^*^*^*^*^*^" << endl;
+
+	return 0;
+
+}
